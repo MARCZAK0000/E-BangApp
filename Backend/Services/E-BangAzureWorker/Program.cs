36f40ebad22c07c8d56228b916de447764009bf8
Marczak Jakub <marczak.j@e80group.com>
2025-03-11 16:24:18 +0100

    Minor Chagnes

--- Backend/Services/E-BangAzureWorker/Program.cs
+++ Backend/Services/E-BangAzureWorker/Program.cs
@@ -1,7 +1,13 @@
+using Azure.Storage.Blobs;
 using E_BangAzureWorker;
+using Microsoft.Extensions.Azure;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
 
+builder.Services.AddSingleton(
+    pr => new BlobServiceClient
+    (builder.Configuration.GetConnectionString("test"),
+    new BlobClientOptions(BlobClientOptions.ServiceVersion.V2020_02_10)));
 var host = builder.Build();
 host.Run();
