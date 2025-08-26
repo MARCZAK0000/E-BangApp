faa799d3b0e7440da037295cb767a47dbad5fd94
Marczak Jakub <marczak.j@e80group.com>
2025-08-26 15:42:01 +0200

    RabbitBuilder Changes

---
+++ Shared/E-BangAppRabbitBuilder/Options/QueueOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_BangAppRabbitBuilder.Options
+{
+    public class QueueOptions
+    {
+        public string Name { get; set; }
+        public string QueueName { get; set; }
+    }
+}
--- Shared/E-BangAppRabbitBuilder/Options/RabbitOptions.cs
+++ Shared/E-BangAppRabbitBuilder/Options/RabbitOptions.cs
@@ -1,24 +1,15 @@
 ﻿namespace E_BangAppRabbitBuilder.Options
 {
-    public class RabbitOptions
+    public class RabbitOptions : RabbitOptionsBase
     {
-        public string Host { get; set; }
-        public int Port { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public string VirtualHost { get; set; }
         public string ListenerQueueName { get; set; }
         public string SenderQueueName { get; set; }
 
         public override string ToString()
         {
-            return $"{nameof(Host)}: {Host}, " +
-                   $"{nameof(Port)}: {Port}, " +
-                   $"{nameof(UserName)}: {UserName}, " +
-                   $"{nameof(Password)}: {Password}, " +
-                   $"{nameof(VirtualHost)}: {VirtualHost}, " +
-                   $"{nameof(ListenerQueueName)}: {(string.IsNullOrEmpty(ListenerQueueName) ? "none" : ListenerQueueName)}, " +
-                   $"{nameof(SenderQueueName)}: {(string.IsNullOrEmpty(SenderQueueName) ? "none" : SenderQueueName)}";
+            return base.ToString() +
+                $"{nameof(ListenerQueueName)}: {(string.IsNullOrEmpty(ListenerQueueName) ? "none" : ListenerQueueName)}, " +
+                $"{nameof(SenderQueueName)}: {(string.IsNullOrEmpty(SenderQueueName) ? "none" : SenderQueueName)}";
         }
     }
 }
