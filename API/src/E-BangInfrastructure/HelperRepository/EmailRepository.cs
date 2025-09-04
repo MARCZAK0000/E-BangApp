23660f117526422d9837de670d0d06e1a039fa79
Jakub Marczak <jj.marczak98@gmail.com>
2025-09-04 21:15:13 +0200

    Email Repository Changes

--- API/src/E-BangInfrastructure/HelperRepository/EmailRepository.cs
+++ API/src/E-BangInfrastructure/HelperRepository/EmailRepository.cs
@@ -1,9 +1,12 @@
-﻿using E_BangAppRabbitSharedClass.BuildersDto.Header;
-using E_BangAppRabbitSharedClass.BuildersDto.Body;
-using E_BangAppRabbitSharedClass.RabbitModel;
-using E_BangDomain.ModelDtos.MessageSender;
-using E_BangDomain.HelperRepository;
+﻿using E_BangAppRabbitSharedClass.BuildersDto.Body.BodyBuilder;
 using E_BangAppRabbitSharedClass.BuildersDto.Footer;
+using E_BangAppRabbitSharedClass.BuildersDto.Header;
+using E_BangAppRabbitSharedClass.BuildersDto.RabbitMessageChilds;
+using E_BangAppRabbitSharedClass.Enums;
+using E_BangAppRabbitSharedClass.RabbitModel;
+using E_BangDomain.HelperRepository;
+using E_BangDomain.ModelDtos.MessageSender;
+using System.Text.Json;
 
 namespace E_BangInfrastructure.HelperRepository
 {
@@ -18,30 +21,32 @@ namespace E_BangInfrastructure.HelperRepository
 
         public async Task SendEmailConfirmAccountAsync(string token, string email, CancellationToken cancellationToken)
         {
-            var buildEmail = new EmailServiceRabbitMessageModel()
+            var buildEmail = new EmailBody()
             {
-                AddressTo = email,
+                AdressedTo = email,
                 Subject = "Confirm Email",
-                Body = new()
+                Header = new HeaderDefaultTemplateBuilder
                 {
-                    Header = new HeaderDefaultTemplateBuilder()
-                    {
-                        Email = email,
-                    },
-                    Body = new ConfirmEmailTokenBodyBuilder()
-                    {
-                        Email = email,
-                        Token = token,
-                    },
-                    Footer = new FooterDefualtTemplateBuilder()
-                    {
-                        Year = DateTime.Now.Year.ToString()
-                    }
-                }
+                    Email = email,
+                },
+                Footer = new FooterDefualtTemplateBuilder()
+                {
+                    Year = DateTime.Now.Year.ToString(),
+                },
+                Body = JsonSerializer.SerializeToElement(new ConfirmEmailTokenBodyBuilder()
+                {
+                    Email = email,
+                    Token = token
+                })
+
             };
             var rabbitMessageDto = new RabbitMessageBaseDto<EmailServiceRabbitMessageModel>()
             {
-                Message = buildEmail,
+                Message = new EmailServiceRabbitMessageModel()
+                {
+                    EEnumEmailBodyBuilderType = EEnumEmailBodyBuilderType.ConfirmEmail,
+                    Message = JsonSerializer.SerializeToElement(buildEmail)
+                },
                 RabbitChannel = E_BangDomain.Enums.ERabbitChannel.EmailChannel
             };
             await _rabbitSenderRepository.AddMessageToQueue(rabbitMessageDto, cancellationToken);
@@ -49,26 +54,26 @@ namespace E_BangInfrastructure.HelperRepository
 
         public async Task SendForgetPasswordTokenEmailAsync(string token, string email, CancellationToken cancellationToken)
         {
-            var buildEmail = new EmailServiceRabbitMessageModel()
+            var buildEmail = new EmailBody()
             {
-                AddressTo = email,
+                AdressedTo = email,
                 Subject = "Forget Password Token",
-                Body = new()
+                Header = new HeaderDefaultTemplateBuilder
                 {
-                    Header = new HeaderDefaultTemplateBuilder
-                    {
-                        Email = email,  
-                    },
-                    //Body
-                    Footer = new FooterDefualtTemplateBuilder()
-                    {
-                        Year = DateTime.Now.Year.ToString(),
-                    }
+                    Email = email,
+                },
+                Footer = new FooterDefualtTemplateBuilder()
+                {
+                    Year = DateTime.Now.Year.ToString(),
                 }
             };
             var rabbitMessageDto = new RabbitMessageBaseDto<EmailServiceRabbitMessageModel>()
             {
-                Message = buildEmail,
+                Message = new EmailServiceRabbitMessageModel()
+                {
+                    EEnumEmailBodyBuilderType = EEnumEmailBodyBuilderType.Registration,
+                    Message = JsonSerializer.SerializeToElement(buildEmail)
+                },
                 RabbitChannel = E_BangDomain.Enums.ERabbitChannel.EmailChannel
             };
             await _rabbitSenderRepository.AddMessageToQueue(rabbitMessageDto, cancellationToken);
@@ -76,60 +81,65 @@ namespace E_BangInfrastructure.HelperRepository
 
         public async Task SendRegistrationConfirmAccountEmailAsync(string token, string email, CancellationToken cancellationToken)
         {
-            var buildEmail = new EmailServiceRabbitMessageModel()
+            var buildEmail = new EmailBody()
             {
-                AddressTo = email,
-                Subject = "Account Registration",
-                Body = new()
+                AdressedTo = email,
+                Subject = "Registration Account",
+                Header = new HeaderDefaultTemplateBuilder
                 {
-                    Header = new HeaderDefaultTemplateBuilder()
-                    {
-                        Email = email,
-                    },
-                    Body = new RegistrationBodyBuilder()
-                    {
-                        Email = email,
-                        Token = token
-                    },
-                    Footer = new()
-                    {
-                        Year = DateTime.Now.Year.ToString()
-                    }
-                }
+                    Email = email,
+                },
+                Footer = new FooterDefualtTemplateBuilder()
+                {
+                    Year = DateTime.Now.Year.ToString(),
+                },
+                Body = JsonSerializer.SerializeToElement(new RegistrationBodyBuilder()
+                {
+                    Email = email,
+                    Token = token
+                })
+
             };
             var rabbitMessageDto = new RabbitMessageBaseDto<EmailServiceRabbitMessageModel>()
             {
-                Message = buildEmail,
+                Message = new EmailServiceRabbitMessageModel()
+                {
+                    EEnumEmailBodyBuilderType = EEnumEmailBodyBuilderType.ConfirmEmail,
+                    Message = JsonSerializer.SerializeToElement(buildEmail)
+                },
                 RabbitChannel = E_BangDomain.Enums.ERabbitChannel.EmailChannel
             };
+
             await _rabbitSenderRepository.AddMessageToQueue(rabbitMessageDto, cancellationToken);
         }
         public async Task SendTwoWayTokenEmailAsync(string token, string email, CancellationToken cancellationToken)
         {
-            var buildEmail = new EmailServiceRabbitMessageModel()
+            var buildEmail = new EmailBody()
             {
-                AddressTo = email,
-                Subject = "Two-Way Authentication Token",
-                Body = new()
+                AdressedTo = email,
+                Subject = "Two Way Token",
+                Header = new HeaderDefaultTemplateBuilder
                 {
-                    Header = new HeaderDefaultTemplateBuilder()
-                    {
-                        Email = email,
-                    },
-                    Body = new TwoWayTokenBodyBuilder()
-                    {
-                        Email = email,
-                        Token = token
-                    },
-                    Footer = new()
-                    {
-                        Year = DateTime.Now.Year.ToString()
-                    }
-                }
+                    Email = email,
+                },
+                Footer = new FooterDefualtTemplateBuilder()
+                {
+                    Year = DateTime.Now.Year.ToString(),
+                },
+                Body = JsonSerializer.SerializeToElement(new TwoWayTokenBodyBuilder()
+                {
+                    Email = email,
+                    Token = token
+                })
+
             };
             var rabbitMessageDto = new RabbitMessageBaseDto<EmailServiceRabbitMessageModel>()
             {
-                Message = buildEmail,
+                Message = new EmailServiceRabbitMessageModel()
+                {
+                    EEnumEmailBodyBuilderType = EEnumEmailBodyBuilderType.ConfirmEmail,
+                    Message = JsonSerializer.SerializeToElement(buildEmail)
+                },
                 RabbitChannel = E_BangDomain.Enums.ERabbitChannel.EmailChannel
             };
             await _rabbitSenderRepository.AddMessageToQueue(rabbitMessageDto, cancellationToken);
