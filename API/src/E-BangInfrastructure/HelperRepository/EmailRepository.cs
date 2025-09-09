5c67d45fc744b561cb8df326dc7a6b2ea5321acf
Marczak Jakub <marczak.j@e80group.com>
2025-09-09 16:50:16 +0200

    EmailComponentBuilder

--- API/src/E-BangInfrastructure/HelperRepository/EmailRepository.cs
+++ API/src/E-BangInfrastructure/HelperRepository/EmailRepository.cs
@@ -1,13 +1,11 @@
-﻿using E_BangAppRabbitSharedClass.BuildersDto.Body.BodyBuilder;
-using E_BangAppRabbitSharedClass.BuildersDto.Footer;
-using E_BangAppRabbitSharedClass.BuildersDto.Header;
-using E_BangAppRabbitSharedClass.BuildersDto.RabbitMessageChilds;
-using E_BangAppRabbitSharedClass.Enums;
-using E_BangAppRabbitSharedClass.RabbitModel;
-using E_BangDomain.HelperRepository;
+﻿using E_BangDomain.HelperRepository;
 using E_BangDomain.ModelDtos.MessageSender;
 using System.Text.Json;
-
+using App.EmailHelper.EmailParameters.Body;
+using App.EmailHelper.EmailParameters.Header;
+using App.EmailHelper.EmailParameters.Footer;
+using App.RabbitSharedClass.Email;
+using App.EmailHelper.Shared.Enums;
 namespace E_BangInfrastructure.HelperRepository
 {
     public class EmailRepository : IEmailRepository
@@ -21,32 +19,35 @@ namespace E_BangInfrastructure.HelperRepository
 
         public async Task SendEmailConfirmAccountAsync(string token, string email, CancellationToken cancellationToken)
         {
-            var buildEmail = new EmailBody()
-            {
-                AdressedTo = email,
-                Subject = "Confirm Email",
-                Header = new HeaderDefaultTemplateBuilder
-                {
-                    Email = email,
-                },
-                Footer = new FooterDefualtTemplateBuilder()
-                {
-                    Year = DateTime.Now.Year.ToString(),
-                },
-                Body = JsonSerializer.SerializeToElement(new ConfirmEmailTokenBodyBuilder()
-                {
-                    Email = email,
-                    Token = token
-                })
 
-            };
-            var rabbitMessageDto = new RabbitMessageBaseDto<EmailServiceRabbitMessageModel>()
+            ConfimEmailParameters confimEmailParameters = new()
             {
-                Message = new EmailServiceRabbitMessageModel()
-                {
-                    EEnumEmailBodyBuilderType = EEnumEmailBodyBuilderType.ConfirmEmail,
-                    Message = JsonSerializer.SerializeToElement(buildEmail)
-                },
+                Email = email,
+                Token = token
+            };
+            DefaultHeaderParameters defaultHeaderParameters = new()
+            {
+                AppName = "E-Bang",
+            };
+            DefaultFooterParameters defaultFooterParameters = new()
+            {
+                Year = DateTime.Now.Year.ToString(),
+                AppName = "E-Bang"
+            };
+
+            EmailComponent<DefaultHeaderParameters, ConfimEmailParameters, DefaultFooterParameters> buildEmail =
+                EmailComponent<DefaultHeaderParameters, ConfimEmailParameters, DefaultFooterParameters>.Builder()
+                .WithHeader(EEmailHeaderType.Default,defaultHeaderParameters)
+                .WithFooter(EEmailFooterType.Defualt, defaultFooterParameters)
+                .WithBody(EEmailBodyType.ConfirmEmail,confimEmailParameters)
+                .WithSubject("Confirm your email")
+                .WithAddressTo(email)
+                .Build();
+
+
+            var rabbitMessageDto = new RabbitMessageBaseDto<EmailComponent<DefaultHeaderParameters, ConfimEmailParameters, DefaultFooterParameters>>()
+            {
+                Message = buildEmail,
                 RabbitChannel = E_BangDomain.Enums.ERabbitChannel.EmailChannel
             };
             await _rabbitSenderRepository.AddMessageToQueue(rabbitMessageDto, cancellationToken);
@@ -54,92 +55,96 @@ namespace E_BangInfrastructure.HelperRepository
 
         public async Task SendForgetPasswordTokenEmailAsync(string token, string email, CancellationToken cancellationToken)
         {
-            var buildEmail = new EmailBody()
-            {
-                AdressedTo = email,
-                Subject = "Forget Password Token",
-                Header = new HeaderDefaultTemplateBuilder
-                {
-                    Email = email,
-                },
-                Footer = new FooterDefualtTemplateBuilder()
-                {
-                    Year = DateTime.Now.Year.ToString(),
-                }
-            };
-            var rabbitMessageDto = new RabbitMessageBaseDto<EmailServiceRabbitMessageModel>()
-            {
-                Message = new EmailServiceRabbitMessageModel()
-                {
-                    EEnumEmailBodyBuilderType = EEnumEmailBodyBuilderType.Registration,
-                    Message = JsonSerializer.SerializeToElement(buildEmail)
-                },
-                RabbitChannel = E_BangDomain.Enums.ERabbitChannel.EmailChannel
-            };
-            await _rabbitSenderRepository.AddMessageToQueue(rabbitMessageDto, cancellationToken);
+            //var buildEmail = new EmailBody()
+            //{
+            //    AdressedTo = email,
+            //    Subject = "Forget Password Token",
+            //    Header = new HeaderDefaultTemplateBuilder
+            //    {
+            //        Email = email,
+            //    },
+            //    Footer = new FooterDefualtTemplateBuilder()
+            //    {
+            //        Year = DateTime.Now.Year.ToString(),
+            //    }
+            //};
+            //var rabbitMessageDto = new RabbitMessageBaseDto<EmailServiceRabbitMessageModel>()
+            //{
+            //    Message = new EmailServiceRabbitMessageModel()
+            //    {
+            //        EEnumEmailBodyBuilderType = EEnumEmailBodyBuilderType.Registration,
+            //        Message = JsonSerializer.SerializeToElement(buildEmail)
+            //    },
+            //    RabbitChannel = E_BangDomain.Enums.ERabbitChannel.EmailChannel
+            //};
+            //await _rabbitSenderRepository.AddMessageToQueue(rabbitMessageDto, cancellationToken);
+            throw new NotImplementedException();
         }
 
         public async Task SendRegistrationConfirmAccountEmailAsync(string token, string email, CancellationToken cancellationToken)
         {
-            var buildEmail = new EmailBody()
+            RegistrationAccountParameters confimEmailParameters = new()
             {
-                AdressedTo = email,
-                Subject = "Registration Account",
-                Header = new HeaderDefaultTemplateBuilder
-                {
-                    Email = email,
-                },
-                Footer = new FooterDefualtTemplateBuilder()
-                {
-                    Year = DateTime.Now.Year.ToString(),
-                },
-                Body = JsonSerializer.SerializeToElement(new RegistrationBodyBuilder()
-                {
-                    Email = email,
-                    Token = token
-                })
-
+                Email = email,
+                Token = token
             };
-            var rabbitMessageDto = new RabbitMessageBaseDto<EmailServiceRabbitMessageModel>()
+            DefaultHeaderParameters defaultHeaderParameters = new()
             {
-                Message = new EmailServiceRabbitMessageModel()
-                {
-                    EEnumEmailBodyBuilderType = EEnumEmailBodyBuilderType.ConfirmEmail,
-                    Message = JsonSerializer.SerializeToElement(buildEmail)
-                },
+                AppName = "E-Bang",
+            };
+            DefaultFooterParameters defaultFooterParameters = new()
+            {
+                Year = DateTime.Now.Year.ToString(),
+                AppName = "E-Bang"
+            };
+
+            EmailComponent<DefaultHeaderParameters, RegistrationAccountParameters, DefaultFooterParameters> buildEmail =
+                EmailComponent<DefaultHeaderParameters, RegistrationAccountParameters, DefaultFooterParameters>.Builder()
+                .WithHeader(EEmailHeaderType.Default, defaultHeaderParameters)
+                .WithFooter(EEmailFooterType.Defualt, defaultFooterParameters)
+                .WithBody(EEmailBodyType.Registration, confimEmailParameters)
+                .WithSubject("Confirm your email")
+                .WithAddressTo(email)
+                .Build();
+
+
+            var rabbitMessageDto = new RabbitMessageBaseDto<EmailComponent<DefaultHeaderParameters, RegistrationAccountParameters, DefaultFooterParameters>>()
+            {
+                Message = buildEmail,
                 RabbitChannel = E_BangDomain.Enums.ERabbitChannel.EmailChannel
             };
-
             await _rabbitSenderRepository.AddMessageToQueue(rabbitMessageDto, cancellationToken);
         }
         public async Task SendTwoWayTokenEmailAsync(string token, string email, CancellationToken cancellationToken)
         {
-            var buildEmail = new EmailBody()
+            TwoWayTokenParameters confimEmailParameters = new()
             {
-                AdressedTo = email,
-                Subject = "Two Way Token",
-                Header = new HeaderDefaultTemplateBuilder
-                {
-                    Email = email,
-                },
-                Footer = new FooterDefualtTemplateBuilder()
-                {
-                    Year = DateTime.Now.Year.ToString(),
-                },
-                Body = JsonSerializer.SerializeToElement(new TwoWayTokenBodyBuilder()
-                {
-                    Email = email,
-                    Token = token
-                })
-
+                Email = email,
+                Token = token
             };
-            var rabbitMessageDto = new RabbitMessageBaseDto<EmailServiceRabbitMessageModel>()
+            DefaultHeaderParameters defaultHeaderParameters = new()
             {
-                Message = new EmailServiceRabbitMessageModel()
-                {
-                    EEnumEmailBodyBuilderType = EEnumEmailBodyBuilderType.ConfirmEmail,
-                    Message = JsonSerializer.SerializeToElement(buildEmail)
-                },
+                AppName = "E-Bang",
+            };
+            DefaultFooterParameters defaultFooterParameters = new()
+            {
+                Year = DateTime.Now.Year.ToString(),
+                AppName = "E-Bang"
+            };
+
+            EmailComponent<DefaultHeaderParameters, TwoWayTokenParameters, DefaultFooterParameters> buildEmail =
+                EmailComponent<DefaultHeaderParameters, TwoWayTokenParameters, DefaultFooterParameters>.Builder()
+                .WithHeader(EEmailHeaderType.Default, defaultHeaderParameters)
+                .WithFooter(EEmailFooterType.Defualt, defaultFooterParameters)
+                .WithBody(EEmailBodyType.ConfirmEmail, confimEmailParameters)
+                .WithSubject("Confirm your email")
+                .WithAddressTo(email)
+                .Build();
+
+
+            var rabbitMessageDto = new RabbitMessageBaseDto<EmailComponent<DefaultHeaderParameters, TwoWayTokenParameters, DefaultFooterParameters>>()
+            {
+                Message = buildEmail,
                 RabbitChannel = E_BangDomain.Enums.ERabbitChannel.EmailChannel
             };
             await _rabbitSenderRepository.AddMessageToQueue(rabbitMessageDto, cancellationToken);
