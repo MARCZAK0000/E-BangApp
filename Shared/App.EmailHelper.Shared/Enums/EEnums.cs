3fa1736604729cfed64f911329dd1c5858a7e623
Marczak Jakub <marczak.j@e80group.com>
2025-09-09 11:33:56 +0200

    Version 2.0.6 - Create new project to share enum - types of message

---
+++ Shared/App.EmailHelper.Shared/Enums/EEnums.cs
@@ -0,0 +1,20 @@
+﻿namespace App.EmailHelper.Shared.Enums
+{
+    public enum HeaderEnum
+    {
+        Defualt = 0,
+        Custom = 1
+    }
+    public enum EEnumEmailBodyBuilderType
+    {
+        Registration = 0,
+        ConfirmEmail = 1,
+        ChangePassword = 2,
+        TwoWayToken = 3,
+    }
+    public enum FooterEnum
+    {
+        Defualt = 0,
+        Custom = 1
+    }
+}
